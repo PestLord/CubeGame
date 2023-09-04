14279990612ec11db53b4e3a2ed124ed287a0a88
PestLord <swords_keeper@mail.ru>
2023-09-05 01:23:36 +0600

    Initial commit

---
+++ Assets/Задания/Задача 1/Level1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Level1 : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0;
+    
+    private void Update()
+    {
+        var step = Time.deltaTime * _speed;
+        var transformPosition = transform.position;
+        transformPosition.x += step;
+
+        transform.position = transformPosition;
+    }
+}
